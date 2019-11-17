04ec6abfc870874aef57c165f8d5957cc1eed53a
swsong89 <ssw890720@gmail.com>
2019-11-17 16:48:22 +0900

    1.blog class 코드 리팩토링

--- miniProject/src/main.js
+++ miniProject/src/main.js
@@ -1,68 +1,58 @@
 class Blog {
   constructor() {
-    //블로그 리스트를 불러오는 함수
     this.setInitValues();
-    this.registerEvent();
+    this.eventRegister();
   }
 
   setInitValues() {
+    this.listBlogUl = document.querySelector('.listBlog > ul');
     this.likeSet = new Set();
-    this.blogList = document.querySelector('.blogList > ul');
   }
 
-  registerEvent() {
+  eventRegister() {
+    this.listBlogEvent();
+    this.likeEvent();
+  }
+
+  // 블로그 리스트 이벤트
+  listBlogEvent() {
     const blogBtn = document.querySelector('.start');
-    const dataUrl = '/data/data.json' ; //  블로그 데이터가 있는 url
+    const dataUrl = '/data/data.json';
 
-    // 블로그 버튼 이벤트
-    blogBtn.addEventListener('click', () => {
-      this.setInitData(dataUrl); // 블로그 목록정보 세팅
-    });
-    
-    // 블로그 목록 이벤트
-    this.blogList.addEventListener('click', ({target}) => {
-      // 좋아요 이벤트
-      this.likeEvent(target);
+    blogBtn.addEventListener('click', () =>{
+      this.getData(dataUrl, this.makeBlogList.bind(this));
     });
   }
 
-  setInitData(dataUrl) {    
-    // bind 하지 않의면 insetBlogList의 내부에서 this는 window 객체가 호출됨
-    this.getData(dataUrl, this.insertBlogList.bind(this));
-  }
-
+  //블로그 데이터 조회
   getData(dataUrl, fn) {
     const oReq = new XMLHttpRequest();
-    oReq.addEventListener('load', ({target}) => {
-      //oReq.responseText 와 동일하다
-      let list = JSON.parse(target.responseText).body;
-      fn(list);
+    oReq.addEventListener('load', () => {
+      const data = JSON.parse(oReq.responseText).body;
+      fn(data);
     });
 
     oReq.open('GET', dataUrl);
     oReq.send();
   }
 
-  insertBlogList(list) {
-    let lis = '';
-    list.forEach( v => {
-      lis += `
+  //블로그 목록 생성
+  makeBlogList(data) {
+    let listBlog = '';
+    data.forEach( v => {
+      listBlog +=`
         <li>
           <a href="${v.link}">${v.title}</a>
-          <div class="like">좋아요</div>
+          <div class="like" >좋아요</div>
         </li>
-      `;
+      `
     });
     // debugger;
-    this.blogList.innerHTML = lis; 
+    this.listBlogUl.innerHTML = listBlog;
   }
 
-  likeEvent(target) {
-    const className = target.className;
-    if (className ==! 'like' && className ==! 'unlike') return;
-
-    const title = target.previousElementSibling.textContent;
-    
+  //좋아요 이벤트
+  likeEvent() {
     /**
      * 클릭한 좋아요의 클래스가 like이면 
      * 1.싫어요로 글자를 변경
@@ -71,30 +61,40 @@ class Blog {
      * 
      * unlike면 반대로 작동
      */
-    if (className ==='like') {
-      target.className = 'unlike';
-      target.innerHTML = '싫어요';
-      this.likeSet.add(title);
-    } else {
-      target.className = 'like';
-      target.innerHTML = '좋아요';
-      this.likeSet.delete(title);
-    }
+    this.listBlogUl.addEventListener('click', ({target}) => {
+      const className = target.className ;
+      if (className !== 'like' &&  className !== 'unlike') return;
 
-    // 찜목록 업데이트
-    this.updateLikeList();
+      this.changeLikeState(target, className);  
+      this.makeLikeList();
+    });
   }
 
-  updateLikeList() {
+  changeLikeState(target, className) {
+      const title = target.previousElementSibling.innerHTML;
+      
+      if(className === 'like') {
+        target.className = 'unlike';
+        target.textContent = '싫어요';
+        this.likeSet.add(title);
+      } 
+      else if (className === 'unlike') {
+        target.className = 'like';
+        target.textContent = '좋아요';
+        this.likeSet.delete(title);
+      }
+  }
+
+  makeLikeList() {
     let lis = '';
-    this.likeSet.forEach( (v) => {
-      lis +=`
+    this.likeSet.forEach( v => {
+    lis += `
         <li>${v}</li>
       `
     });
-    
-    const likeList = document.querySelector('.likeList > ul');
-    likeList.innerHTML = lis;
+    const listLike = document.querySelector('.listLike > ul');
+    listLike.innerHTML = lis;
+    lis
   }
 }
 
